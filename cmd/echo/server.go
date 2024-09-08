f39cc07375a75a52561ddf34042953622c0ae3a2
nopecho <ch.jooon@gmail.com>
2024-09-09 01:07:19 +0900

    example

---
+++ cmd/echo/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/nopecho/golang-template/internal/app/config"
+	"github.com/nopecho/golang-template/pkg/echoserver"
+	"github.com/nopecho/golang-template/pkg/synk"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	e := echoserver.NewEcho()
+
+	e.GET("goroutine", func(c echo.Context) error {
+		format := echoserver.DefaultQueryParam(c, "format", "json")
+		sizeParam := echoserver.DefaultQueryParam(c, "size", "1000")
+		size, _ := strconv.Atoi(sizeParam)
+
+		cm := synk.OpenBufferChannel[string](size)
+		minN := 0
+		maxN := 3
+		go func() {
+			for i := range cm.Size {
+				go func() {
+					time.Sleep(time.Duration(rand.Intn(maxN-minN+1)+minN) * time.Second)
+					cm.Send(fmt.Sprintf("test %d sequence", i))
+				}()
+			}
+		}()
+		result := cm.WaitReceive()
+
+		switch format {
+		case "json":
+			return c.JSON(http.StatusOK, result)
+		case "csv":
+			data := echoserver.NewCsvData([]string{"idx", "result"})
+			for i, v := range result {
+				data = echoserver.AppendCsvData(data, []string{strconv.Itoa(i), v})
+			}
+			return echoserver.CsvResponse(c, "goroutine", data)
+		default:
+			return c.JSON(http.StatusBadRequest, echoserver.Map{
+				"error": "invalid format",
+			})
+		}
+	})
+
+	echoserver.Run(e, config.Env.Port)
+}
--- pkg/echoserver/util.go
+++ pkg/echoserver/util.go
@@ -1,3 +1,47 @@
 package echoserver
 
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
 type Map map[string]any
+
+type CsvData [][]string
+
+func DefaultQueryParam(c echo.Context, key string, defaultValue string) string {
+	value := c.QueryParam(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func CsvResponse(c echo.Context, name string, data CsvData) error {
+	disposition := fmt.Sprintf("attachment; filename=%s.csv", name)
+	c.Response().Header().Set(echo.HeaderContentType, "text/csv")
+	c.Response().Header().Set(echo.HeaderContentDisposition, disposition)
+
+	writer := csv.NewWriter(c.Response().Writer)
+	for _, row := range data {
+		if err := writer.Write(row); err != nil {
+			log.Warn().Msgf("error writing record to csv: %v, err: %v", row, err)
+		}
+	}
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		_ = c.String(http.StatusInternalServerError, fmt.Sprintf("error writing csv: %v", err))
+	}
+	return c.NoContent(http.StatusOK)
+}
+
+func NewCsvData(header []string) CsvData {
+	return [][]string{header}
+}
+
+func AppendCsvData(data CsvData, row []string) CsvData {
+	return append(data, row)
+}
